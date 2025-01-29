a3095a263b63b7170b352ee339c8405e50b45275
RicardBalbino <ricardobalbinovazfilho@hotmail.com>
2025-01-28 21:47:06 -0300

    Add Função adicionarAmigo() + limparCampo

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigo = [];
+
+function limparTexto() {
+    let campo = document.querySelector('input');
+    campo.innerHTML = '';
+}
