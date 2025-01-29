91bc3b434a3e50ceea92543792d6fddb57589e49
RicardBalbino <ricardobalbinovazfilho@hotmail.com>
2025-01-29 11:02:18 -0300

    Atualizacao

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-let amigo = [];
+let listaAmigoSecreto = [];
 
-function limparTexto() {
-    let campo = document.querySelector('input');
-    campo.innerHTML = '';
+function adicionarAmigo(){
+    let nomeAmigoSecreto = document.getElementById('amigo').value;
+    
+    if (nomeAmigoSecreto === ''){
+        alert('Erro: O campo não pode estar vazio !');
+        return;
+    } 
+    listaAmigoSecreto.push(nomeAmigoSecreto);
+    console.log('Nome adicionado a lista');    
+    console.log(listaAmigoSecreto);
+    limparCampo();
+}
+
+function limparCampo(){
+    nomeAmigoSecreto = document.getElementById('amigo');
+    nomeAmigoSecreto.value = '';
 }
